286a6c1c4bb27d5bdb15daee22b87c5dafb70866
danielfellner <fellnerdanielferenc@gmail.com>
2024-02-26 16:24:34 +0100

    correct Sonar issues

--- src/main/java/hu/ponte/hr/controller/upload/UploadController.java
+++ src/main/java/hu/ponte/hr/controller/upload/UploadController.java
@@ -3,11 +3,16 @@ package hu.ponte.hr.controller.upload;
 import hu.ponte.hr.interfaces.ImageStoreServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.spec.InvalidKeySpecException;
 
 @Component
 @RequestMapping("api/file")
@@ -20,9 +25,12 @@ public class UploadController {
     this.imageStoreService = imageStoreService;
   }
 
-  @RequestMapping(value = "post", method = RequestMethod.POST)
+  @PostMapping("post")
   @ResponseBody
-  public String handleFormUpload(@RequestParam("file") MultipartFile file) {
+  public String handleFormUpload(@RequestParam("file") MultipartFile file)
+      throws IOException, CertificateException, NoSuchAlgorithmException, UnrecoverableKeyException,
+          InvalidKeyException, SignatureException, NoSuchPaddingException, BadPaddingException,
+          KeyStoreException, InvalidKeySpecException, IllegalBlockSizeException {
     this.imageStoreService.storeImage(file);
     return "ok";
   }
--- src/test/java/hu/ponte/hr/PonteTestApplicationTests.java
+++ src/test/java/hu/ponte/hr/PonteTestApplicationTests.java
@@ -1,14 +1,26 @@
 package hu.ponte.hr;
 
+import hu.ponte.hr.config.AppConfig;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class PonteTestApplicationTests {
+  @Autowired AppConfig appConfig;
 
   @Test
-  public void contextLoads() {}
+  public void contextLoads() {
+    long maxUploadPhotoSize = 2097152;
+    String signServiceFactoryKey = "RSA";
+    String signServiceSignature = "SHA256withRSA";
+
+    Assert.assertEquals(maxUploadPhotoSize, appConfig.getMaxUploadPhotoSize());
+    Assert.assertEquals(signServiceFactoryKey, appConfig.getSignServiceFactoryKey());
+    Assert.assertEquals(signServiceSignature, appConfig.getSignServiceSignature());
+  }
 }
